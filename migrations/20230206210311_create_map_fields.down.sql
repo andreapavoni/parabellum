da48fffa87ec2cdfc99a9d4f8cfc2e35c46546dc
Andrea Pavoni <andrea.pavoni@gmail.com>
2024-11-27 22:50:21 +0100

    remove sql migrations

--- migrations/20230206210311_create_map_fields.down.sql
+++
@@ -1,4 +0,0 @@
--- Add down migration script here
-DROP TABLE IF EXISTS map_fields;
-DROP INDEX IF EXISTS idx_map_fields_id;
-DROP INDEX IF EXISTS idx_map_fields_position;
--- migrations/20230206210320_create_players.down.sql
+++
@@ -1,3 +0,0 @@
--- Add down migration script here
-DROP TABLE IF EXISTS players;
-DROP INDEX IF EXISTS idx_players_username;
