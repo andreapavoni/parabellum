5642d1a64dd37519ca2a2ffe791d12fc1aad5d9e
Andrea Pavoni <andrea.pavoni@gmail.com>
2025-10-09 18:51:01 +0200

    wip: removing cqrs_es for simpler architecture

--- src/app/commands/mod.rs
+++ src/app/commands/mod.rs
@@ -1,49 +1,26 @@
-pub mod attack;
 pub mod register_player;
 
-use anyhow::Result;
-
-use super::events::GameEvent;
-use crate::game::{
-    battle::CataTargets,
-    models::{army::Army, Tribe},
-};
-
-#[async_trait::async_trait]
-pub trait Command {
-    fn validate(&self) -> Result<()> {
-        Ok(())
-    }
-    async fn run(&self) -> Result<Vec<GameEvent>>;
-}
-
-#[derive(Debug, Clone)]
-pub enum Cmd {
-    RegisterPlayer {
-        username: String,
-        tribe: Tribe,
-    },
-    Attack {
-        village_id: u32,
-        army: Army,
-        cata_targets: CataTargets,
-        defender_map_id: u32,
-    },
-    Raid,
-    Reinforce,
-    ReturnArmy,
-    SendMerchant,
-    ReturnMerchant,
-    TrainBarracksUnit,
-    TrainStableUnit,
-    TrainWorkshopUnit,
-    TrainExpansionUnit,
-    TrainTrapperUnit,
-    TrainGreatBarracksUnit,
-    TrainGreatStableUnit,
-    TrainGreatWorkshopUnit,
-    ResearchAcademy,
-    ResearchSmithy,
-    StartTownHallCelebration,
-    StartBreweryCelebration,
-}
+// RegisterPlayer
+// Attack {
+//     village_id: u32,
+//     army: Army,
+//     cata_targets: CataTargets,
+//     defender_map_id: u32,
+// },
+// Raid,
+// Reinforce,
+// ReturnArmy,
+// SendMerchant,
+// ReturnMerchant,
+// TrainBarracksUnit,
+// TrainStableUnit,
+// TrainWorkshopUnit,
+// TrainExpansionUnit,
+// TrainTrapperUnit,
+// TrainGreatBarracksUnit,
+// TrainGreatStableUnit,
+// TrainGreatWorkshopUnit,
+// ResearchAcademy,
+// ResearchSmithy,
+// StartTownHallCelebration,
+// StartBreweryCelebration,
--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,74 +1,22 @@
-use std::sync::Arc;
-
-use anyhow::Result;
-
-use crate::repository::Repository;
-
-use self::{
-    commands::{attack::AttackCommand, register_player::RegisterPlayerCommand, Cmd, Command},
-    consumers::MainConsumer,
-};
-
-pub mod aggregates;
 pub mod commands;
-pub mod consumers;
-pub mod events;
 pub mod jobs;
 
-pub struct App {
-    repo: Arc<dyn Repository>,
-}
-
-impl App {
-    pub fn new(repo: Arc<dyn Repository>) -> Self {
-        Self { repo }
-    }
-
-    pub async fn command(&self, cmd: Cmd) -> Result<()> {
-        let command: Box<dyn Command> = match cmd {
-            Cmd::RegisterPlayer { username, tribe } => Box::new(RegisterPlayerCommand::new(
-                self.repo.clone(),
-                username,
-                tribe,
-            )),
-            Cmd::Attack {
-                village_id,
-                army,
-                cata_targets,
-                defender_map_id: defender_village_id,
-            } => Box::new(AttackCommand::new(
-                self.repo.clone(),
-                village_id,
-                army.clone(),
-                cata_targets.clone(),
-                defender_village_id,
-            )),
-            Cmd::Raid => todo!(),
-            Cmd::Reinforce => todo!(),
-            Cmd::ReturnArmy => todo!(),
-            Cmd::SendMerchant => todo!(),
-            Cmd::ReturnMerchant => todo!(),
-            Cmd::TrainBarracksUnit => todo!(),
-            Cmd::TrainStableUnit => todo!(),
-            Cmd::TrainWorkshopUnit => todo!(),
-            Cmd::TrainExpansionUnit => todo!(),
-            Cmd::TrainTrapperUnit => todo!(),
-            Cmd::TrainGreatBarracksUnit => todo!(),
-            Cmd::TrainGreatStableUnit => todo!(),
-            Cmd::TrainGreatWorkshopUnit => todo!(),
-            Cmd::ResearchAcademy => todo!(),
-            Cmd::ResearchSmithy => todo!(),
-            Cmd::StartTownHallCelebration => todo!(),
-            Cmd::StartBreweryCelebration => todo!(),
-        };
-
-        // command.validate()?;
-        let events = command.run().await?;
-
-        println!("Produced events -> {:?}", events.clone());
-
-        MainConsumer::process_events(events)?;
-
-        Ok(())
-    }
-}
+// RegisterPlaye
+// Attac
+// Raid
+// Reinforce
+// ReturnArmy
+// SendMerchant
+// ReturnMerchant
+// TrainBarracksUnit
+// TrainStableUnit
+// TrainWorkshopUnit
+// TrainExpansionUnit
+// TrainTrapperUnit
+// TrainGreatBarracksUnit
+// TrainGreatStableUnit
+// TrainGreatWorkshopUnit
+// ResearchAcademy
+// ResearchSmithy
+// StartTownHallCelebration
+// StartBreweryCelebration
