5fdde634a8c26fdbb8e23bdfc8c1b1cb0e392dad
Andrea Pavoni <andrea.pavoni@gmail.com>
2025-11-04 03:50:23 +0100

    refactor local db setup script

--- setup_db.sh
+++ setup_db.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Load environment variables from .env file
 if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 else
@@ -12,15 +10,12 @@ else
 fi
 
 DATABASE_NAME=$(echo $DATABASE_URL | cut -d '/' -f4)
+TEST_DATABASE_NAME=$(echo $TEST_DATABASE_URL | cut -d '/' -f4)
 
-echo "--- Dropping database '$DATABASE_NAME' (if exists) ---"
 docker exec -it parabellum_db dropdb -U parabellum $DATABASE_NAME --if-exists
-
-echo "--- Creating database '$DATABASE_NAME' ---"
 docker exec -it parabellum_db createdb -U parabellum $DATABASE_NAME
-
-echo "--- Running migrations on '$DATABASE_NAME' ---"
-# Explicitly pass the database URL to the command
 sqlx migrate run --database-url "$DATABASE_URL"
 
-echo "--- Database setup complete! ---"
+docker exec -it parabellum_db dropdb -U parabellum $TEST_DATABASE_NAME --if-exists
+docker exec -it parabellum_db createdb -U parabellum $TEST_DATABASE_NAME
+sqlx migrate run --database-url "$TEST_DATABASE_URL"
