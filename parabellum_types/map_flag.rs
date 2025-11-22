ab4204e664dc70da35a9d256eb1feb90af06a4d5
Rodolfo Mota <rodolfomota96@gmail.com>
2025-11-22 09:14:47 +0000

    refactor: clean up.

--- parabellum_types/map_flag.rs
+++ parabellum_types/map_flag.rs
@@ -11,31 +11,3 @@ pub enum MapFlagType {
     /// Type 2: Custom flag - static marker at specific map coordinates with custom text
     CustomFlag = 2,
 }
-
-impl MapFlagType {
-    pub fn from_i16(value: i16) -> Option<Self> {
-        match value {
-            0 => Some(MapFlagType::PlayerMark),
-            1 => Some(MapFlagType::AllianceMark),
-            2 => Some(MapFlagType::CustomFlag),
-            _ => None,
-        }
-    }
-
-    pub fn as_i16(self) -> i16 {
-        self as i16
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_map_flag_type_conversion() {
-        assert_eq!(MapFlagType::from_i16(0).unwrap(), MapFlagType::PlayerMark);
-        assert_eq!(MapFlagType::from_i16(1).unwrap(), MapFlagType::AllianceMark);
-        assert_eq!(MapFlagType::from_i16(2).unwrap(), MapFlagType::CustomFlag);
-        assert!(MapFlagType::from_i16(3).is_none());
-    }
-}
